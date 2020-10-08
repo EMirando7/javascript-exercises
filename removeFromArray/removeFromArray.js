a8f9b5709b84840816d7d0e6a8baf3d670870960
Emiliano Miranda <emirando117@gmail.com>
2020-10-08 07:54:48 -0500

    do 2nd test

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    for(let i = 0 ; i < arr.length;i ++) {
+        //i can access each individual num from nums
 
-}
-
+        let currentNum = nums.shift();
+        if(currentNum !== undefined){
+        arr.splice(arr.indexOf(currentNum), 1);
+        }
+    }
+    
+    return arr;
+    }
 module.exports = removeFromArray
