73945d876dc64e44ebb0b44cdea10bc4fdbabfd6
Emiliano Miranda <emirando117@gmail.com>
2020-10-09 18:57:41 -0500

    add 4th jasmin spec sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -2,6 +2,9 @@ const sumAll = function(num1, num2) {
     debugger;
     let counter = num1;
     let newNum = num1;
+    if(num1 < 0 || num2 < 0){
+        return 'ERROR'
+    }
     if(num1 < num2){
         while (counter < num2) {
             counter++
