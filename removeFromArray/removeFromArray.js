7593e11d9e88a3eee8f97db20f89ea104c6588c7
Emiliano Miranda <emirando117@gmail.com>
2020-10-09 13:34:13 -0500

    4th removeFromArray exercise, change arr.length to nums.length, add decreasing expression instead

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,13 +1,13 @@
 const removeFromArray = function(arr, ...nums) {
-    for(let i = 0 ; i < arr.length;i ++) {
-        //i can access each individual num from nums
-
+    // debugger; [1, 2, 3]
+    for( let counter = nums.length;counter > 0  ;counter --) {
         let currentNum = nums.shift();
         if(currentNum !== undefined && arr.includes(currentNum)){
                 arr.splice(arr.indexOf(currentNum), 1);
         }
     }
-    
     return arr;
-    }
+}
+// removeFromArray([1,2,3],1,2,3)
+
 module.exports = removeFromArray
