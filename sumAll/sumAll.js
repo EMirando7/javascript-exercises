327e7090d43c3db7909a7bfb5671930d5c6fdfb5
Emiliano Miranda <emirando117@gmail.com>
2020-10-09 14:31:57 -0500

    1st sumAll jasmine

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let counter = num1;
+    let newNum = num1;
+    while (counter < num2) {
+        counter++
+        newNum += counter;
+    }
+    return newNum;
 }
 
 module.exports = sumAll
