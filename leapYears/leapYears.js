bce40aee1cacba144d462f0a77b308444fc62d79
Emiliano Miranda <emirando117@gmail.com>
2020-10-09 19:33:54 -0500

    add solution 1st leapYears ft. arrow func

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+//divisible by 4
+//not divisible by 100, unless divisible by 400
+//
+const leapYears = function(year) {
+    return year % 4 == 0 ? true : false
 }
 
 module.exports = leapYears
