daf22b1a9e410de2c60c10cd8f39023a6f403294
Emiliano Miranda <emirando117@gmail.com>
2020-10-11 08:13:26 -0500

    1st tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,6 +1,14 @@
-const ftoc = function() {
+//formula (F − 32) × 5/9 = c
 
+const ftoc = function(farenValue) {
+  debugger;
+  let c = (farenValue - 32) * (5/9);
+
+  return c;
 }
+ftoc(32)
+
+
 
 const ctof = function() {
 
